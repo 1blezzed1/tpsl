b4d24de2b632c6a6f85c63de144bf91d8a992f67
1blezzed1 <143650732+1blezzed1@users.noreply.github.com>
2025-08-07 08:13:44 +0300

    Create info.lua

---
+++ info.lua
@@ -0,0 +1,36 @@
+local AllClans = {
+    ["Shinigami"] = {
+        "Sozosei",
+        "Shihouin",
+        "Urahara",
+        "Yamamoto",
+        "Hyosube",
+        "Zaraki",
+        "Shutara",
+        "Tsunayashiro"
+    },
+    ["Hollow"] = {
+        "Ikomikidomoe",
+        "Starrk",
+        "Odelschwanck",
+        "Louisenbairn",
+        "Jaegerjaquez",
+        "Cifer",
+    },
+    ["Quincy"] = {
+        "Ishida",
+        "Kurosaki",
+        "Du",
+        "Valkyire",
+        "B",
+        "Parnkgjas",
+        "Le vaar"
+    },
+    ["Fullbringer"] = {
+        "Ubuginu",
+        "Orihime",
+        "Sado",
+        "Ginjo",
+        "Tsukishima"
+    },
+}
