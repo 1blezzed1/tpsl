c56c26b20f302ce67558936b684eedd662d68ac9
1blezzed1 <143650732+1blezzed1@users.noreply.github.com>
2025-08-08 04:24:26 +0300

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,38 +1,58 @@
-local AllClans = {
-    ["Shinigami"] = {
-        "Sozosei",
-        "Shihouin",
-        "Urahara",
-        "Yamamoto",
-        "Hyosube",
-        "Zaraki",
-        "Shutara",
-        "Tsunayashiro"
+local Info = {
+    ["All Clans"] = {
+        ["Shinigami"] = {
+            "Sozosei",
+            "Shihouin",
+            "Urahara",
+            "Yamamoto",
+            "Hyosube",
+            "Zaraki",
+            "Shutara",
+            "Tsunayashiro"
+        },
+        ["Hollow"] = {
+            "Ikomikidomoe",
+            "Starrk",
+            "Odelschwanck",
+            "Louisenbairn",
+            "Jaegerjaquez",
+            "Cifer",
+        },
+        ["Quincy"] = {
+            "Ishida",
+            "Kurosaki",
+            "Du",
+            "Valkyire",
+            "B",
+            "Parnkgjas",
+            "Le vaar"
+        },
+        ["Fullbringer"] = {
+            "Ubuginu",
+            "Orihime",
+            "Sado",
+            "Ginjo",
+            "Tsukishima"
+        },
     },
-    ["Hollow"] = {
-        "Ikomikidomoe",
-        "Starrk",
-        "Odelschwanck",
-        "Louisenbairn",
-        "Jaegerjaquez",
-        "Cifer",
-    },
-    ["Quincy"] = {
-        "Ishida",
-        "Kurosaki",
-        "Du",
-        "Valkyire",
-        "B",
-        "Parnkgjas",
-        "Le vaar"
-    },
-    ["Fullbringer"] = {
-        "Ubuginu",
-        "Orihime",
-        "Sado",
-        "Ginjo",
-        "Tsukishima"
-    },
+    ["AllCodes"] = {
+        "setrona1vertagzeu0",
+        "excaliburfool",
+        "higuyscode",
+        "thisiswhywetestthosewhoknow",
+        "somebugsfixes",
+        "goplayranked";
+        "800kcodeyeah",
+        "raidsfixed",
+        "thanksforpatience",
+        "mythoughtsonthislater",
+        "rerererelease",
+        "3daysthosewhoknow",
+        "privateservercompensation",
+        "promiseddecembercode",
+        "compensationforinconvenientrelease",
+        "sorryforthebankbugs"
+    }
 }
 
-return AllClans
+return Info
